e6a2797c84fe55d0418af8749510d4f0010752ba
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2023-02-21 23:52:16 +0000

    Initial Setup + UnrealUtilityAI GitHub

---
+++ UnrealAI/Source/UnrealAI/UnrealAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALAI_API AUnrealAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
